e38724afdf06d9430bfd4ebb2dc50d43c037968a
extrasauceontop <msuchemba@gmail.com>
2021-02-19 12:34:48 -0500

    run

--- scrape.py
+++ scrape.py
@@ -5,6 +5,7 @@ with SgChrome() as driver:
 
   stuff = driver.find_elements_by_xpath(f"//div[@class = 'col-md-4 col-xs-12 pm-location']")
   for element in stuff:
-    print(element.text)
+    e_text = element.text.split("\n")
+    print(e_text[0])
 
 print("done")
