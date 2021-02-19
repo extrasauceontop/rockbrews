786510d532e1eea52c0c66713688d0a840ebeac6
extrasauceontop <msuchemba@gmail.com>
2021-02-19 11:10:50 -0500

    xpath

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ from sgselenium import SgChrome
 with SgChrome() as driver:
   driver.get("https://www.rockandbrews.com/locations")
 
-  stuff = driver.find_element("body")
+  stuff = driver.find_element_by_xpath(f"//div[@class = 'pm-location-search-list']")
 
   print(stuff)
 print("done")
