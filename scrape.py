e5af4539640ade605f42a0eebf59fd01eb245a91
extrasauceontop <msuchemba@gmail.com>
2021-02-19 12:04:36 -0500

    cool

--- scrape.py
+++ scrape.py
@@ -3,7 +3,8 @@ from sgselenium import SgChrome
 with SgChrome() as driver:
   driver.get("https://www.rockandbrews.com/locations")
 
-  stuff = driver.find_element_by_xpath(f"//div[@class = 'pm-location-search-list']").text.split("\n")
+  stuff = driver.find_elements_by_xpath(f"//div[@class = 'pm-location']")
+  for element in stuff:
+    print(element.text)
 
-  print(stuff)
 print("done")
